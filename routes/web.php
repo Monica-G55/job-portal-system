e5765dbc255237d4f21e1ac5b6bea00c7879bbe7
Monica-G55 <monics5553@gmail.com>
2026-05-29 23:16:13 +0530

    processing the update profile

--- routes/web.php
+++ routes/web.php
@@ -33,6 +33,7 @@ Route::group(['account'],function(){
 
           Route::group(['middleware'=>'auth'],function(){
             Route::get('/account/profile',[AccountController::class,'profile'])->name('account.profile');
+            Route::post('/account/update-profile',[AccountController::class,'updateProfilePic'])->name('account.updateProfilePic');
             Route::put('/account/update-profile',[AccountController::class,'updateProfile'])->name('account.updateProfile');
             Route::get('/account/logout',[AccountController::class,'logout'])->name('account.logout');
           });
